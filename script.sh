532d7aadeeae2c0b5cb84c87372c1c9527633f1c
ranebof <117946796+ranebof@users.noreply.github.com>
2023-12-12 18:51:39 +0200

    updated_without_sudo_lab3

--- script.sh
+++ script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install mc vim git httpd cronie -y
-sudo service httpd start
-sudo systemctl start crond
-sudo systemctl enable crond
+yum update -y
+yum install mc vim git httpd cronie -y
+service httpd start
+systemctl start crond
+systemctl enable crond
 
-sudo touch /var/log/sysinfo
-sudo touch /usr/local/bin/sysinfo.sh
-sudo chmod 777 /usr/local/bin/sysinfo.sh
-sudo chmod 644 /var/log/sysinfo
-sudo chmod 644 /etc/crontab
+touch /var/log/sysinfo
+touch /usr/local/bin/sysinfo.sh
+chmod 777 /usr/local/bin/sysinfo.sh
+chmod 644 /var/log/sysinfo
+chmod 644 /etc/crontab
 
 cat << 'EOF' > /usr/local/bin/sysinfo.sh
 #!/bin/bash
@@ -24,4 +24,4 @@ cat << 'EOF' > /usr/local/bin/sysinfo.sh
 EOF
 
 echo "* * * * 1-5 root sudo bash -c "/usr/local/bin/sysinfo.sh"" >> /etc/crontab
-sudo systemctl restart crond
+systemctl restart crond
